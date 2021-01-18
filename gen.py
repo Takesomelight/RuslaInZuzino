9bb461aa61aa189272c8b5db6db95acee88a7767
klaus <riveredrain@gmail.com>
2021-01-19 02:06:52 +0300

    init

--- gen.py
+++ gen.py
@@ -1,21 +1,54 @@
 from random import randint
 import numpy as np
 
+
 class RoomGenerator:
-    def __init__(self, min_w, max_w, min_h, max_h, map_w, map_h):
+    def __init__(self, min_w, max_w, min_h, max_h, margin, map_w, map_h, room_max_iters = 1000):
         self.min_w = min_w
         self.max_w = max_w
         self.min_h = min_h
         self.max_h = max_h
+        self.margin = margin
         self.map_w = map_w
         self.map_h = map_h
+        self.m = np.zeros((self.map_w, self.map_h), np.int)
+        self.i = 0
+        self.rooms = []
+        self.room_max_iters = room_max_iters
 
-    def generate_room(self):
+    def _generate_room(self):
         w = randint(self.min_w, self.max_w)
         h = randint(self.min_h, self.max_h)
         x = randint(0, self.map_w - w)
         y = randint(0, self.map_h - h)
         return (x, y, w, h)
 
-    def generate_rooms(self):
-        np.full(map)
+    def _check_collision(self, room):
+        (x, y, w, h) = room
+        from_x = max(0, x-self.margin)
+        to_x = min(x + w + self.margin, self.map_w - 1)
+        from_y = max(0, y - self.margin)
+        to_y = min(y + h + self.margin, self.map_h - 1)
+        return np.all(self.m[from_x:to_x,from_y:to_y] == 0)
+
+    def _add_room(self, room):
+        (x, y, w, h) = room
+        self.i += 1
+        self.m[x:x+w,y:y+h] = self.i
+        self.rooms.append(room)
+
+    def generate_next_room(self):
+        for it in range(self.room_max_iters):
+            room = self._generate_room()
+            if self._check_collision(room):
+                self._add_room(room)
+                return room
+        return None
+
+    def generate_rooms(self, n):
+        for i in range(n):
+            self.generate_next_room()
+            print("%d room generated" % i)
+        return self.rooms
+
+
--- main.py
+++ main.py
@@ -1,33 +1,46 @@
 from bearlibterminal import terminal
+from gen import RoomGenerator
+import cProfile
+import numpy as np
 
 TITLE = "Rusla in Zuzino Adventure!"
-W = 100 + 2 + 2
-H = 80 + 2 + 2
+W = 100
+H = 80
+ROOMS = 1488
 
 def start_game():
     terminal.open()
     terminal.set("window: size=%dx%d, cellsize=8x8, title='%s'" % (W, H, TITLE))
 
     for x in range(W):
-        terminal.put(x, 0, '#')
-        terminal.put(x, H-1, '#')
-        terminal.put(x, 1, '#')
-        terminal.put(x, H-2, '#')
-    for y in range(H):
-        terminal.put(0, y, '#')
-        terminal.put(W-1, y, '#')
-        terminal.put(1, y, '#')
-        terminal.put(W - 2, y, '#')
+        for y in range(H):
+            put(x, y, '.')
 
+    c = '#'
+
+    for (rx, ry, rw, rh) in generate_rooms():
+        for x in range(rx, rx + rw):
+            put(x, ry, c)
+            put(x, ry + rh - 1, c)
+        for y in range(ry, ry + rh):
+            put(rx, y, c)
+            put(rx + rw - 1, y, c)
     terminal.refresh()
     while terminal.read() != terminal.TK_CLOSE:
-        pass
+        terminal.refresh()
 
     terminal.close()
+
+def put(x, y, c):
+    terminal.put(x, y, c)
+
+def generate_rooms():
+    rg = RoomGenerator(8, 25, 8, 25, 3, W, H)
+    return rg.generate_rooms(ROOMS)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    #cProfile.run('generate_rooms()')
     start_game()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
